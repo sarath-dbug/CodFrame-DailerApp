8ac86c2c27c9fc71e27678fb50a184fd85f8cc29
Sarath Chandran <sarathc.dev@gmail.com>
2025-03-18 16:49:44 +0530

    create get request

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,10 @@ app.get('/', (req,res)=>{
     res.send("WELCOME TO BACKEND");
 });
 
+app.get('/api/auth/login', (req,res)=>{
+    res.send("WELCOME TO Login Page");
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
