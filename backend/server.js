4bb2e94430fd0ab9506725cce921cae88667e8fa
Sarath Chandran <sarathc.dev@gmail.com>
2025-03-19 10:07:35 +0530

    ignore files

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,6 @@ app.get('/', (req,res)=>{
     res.send("WELCOME TO BACKEND");
 });
 
-app.get('/api/auth/login', (req,res)=>{
-    res.send("WELCOME TO Login Page");
-});
-
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
